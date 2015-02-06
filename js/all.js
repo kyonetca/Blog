03282be3d8205c0f9558e15086eeeea5e7dbd88e
Shakna Israel <s4b3r6@gmail.com>
2015-02-06 12:47:40 +1100

    Added today function.

--- js/all.js
+++ js/all.js
@@ -1,3 +1,9 @@
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1; //January is 0!
+var yyyy = today.getFullYear();
+
+
 function setContent() {
 	if (window.name == "")
 	{
@@ -24,3 +30,13 @@ function home() {
 	window.name = "default";
 	setContent();
 }
+
+if(dd<10) {
+    dd='0'+dd
+} 
+
+if(mm<10) {
+    mm='0'+mm
+} 
+
+today = mm+'/'+dd+'/'+yyyy;
